5f05e4a67b0a8cb82bb7b680254978197b1e254d
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2022-12-11 02:59:25 -0500

    tokenization

--- browser-app/src/api/index.ts
+++ browser-app/src/api/index.ts
@@ -1,5 +1,37 @@
 import axios from 'axios';
 import { Article } from '../types/articles';
+import { stemmer } from 'stemmer';
+
+const parseArticle: (content: string) => string = (content: string) => {
+    const html = document.createElement("html");
+    html.innerHTML = content;
+
+    // removes boxes that notify of things like the article not having enough citations.
+    // this information is not relevant to this application, so we ignore it.
+    const ambox = html.getElementsByClassName("ambox");
+    for (let i = 0; i < ambox.length; i++) {
+        ambox[i].parentNode?.removeChild(ambox[i]);
+    }
+    
+    return html.innerHTML;
+}
+
+const tokenizeArticle: (content: string) => string = (content: string) => {
+    // first extract any content explicitly in a "p" tag
+    const html = document.createElement("html");
+    html.innerHTML = content;
+    let text = Array.from(html.getElementsByTagName("p")).map(p => p.innerText).join(" ");
+    // tokenize the text.
+    text =  text.toLowerCase()
+                .replace(/\[.+\]/g, "") // remove references
+                .replace(/\n/g, '') // remove new line characters
+                .replace(/[^\w^\s]/g, '') // remove everything except text and whitespace
+                .replace(/\s5+/g, ' ') // replace any white space with a single space
+                .trim() // remove leading and trailing whitespace
+                .split(' ').map(a => stemmer(a)).join(' '); // apply stemmer
+    console.log(text);
+    return text;
+}
 
 export const randomArticle: () => Promise<Article> = async () => {
     const random = await axios.get(
@@ -12,11 +44,15 @@ export const randomArticle: () => Promise<Article> = async () => {
         `https://en.wikipedia.org/w/api.php?action=parse&page=${title}&prop=text&format=json&redirects=true`
     )
 
+    const content = parse.data.parse.text["*"];
+
+    const body = parseArticle(content);
+    const tokenized = tokenizeArticle(content);
     const categories = await getCategories(title);
 
     const article: Article = {
         title,
-        body: parse.data.parse.text["*"],
+        body,
         categories
     }
 
